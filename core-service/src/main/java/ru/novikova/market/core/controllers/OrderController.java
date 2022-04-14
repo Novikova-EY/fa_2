3c8d51b063684ec60d63718f45497cd59a8df914
sledopitka <sledopitka@gmail.com>
2022-04-14 14:46:32 +0300

    Добавлен Swagger в раздел core

--- core-service/src/main/java/ru/novikova/market/core/controllers/OrderController.java
+++ core-service/src/main/java/ru/novikova/market/core/controllers/OrderController.java
@@ -1,16 +1,31 @@
 package ru.novikova.market.core.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.novikova.market.api.dtos.ProductDto;
 import ru.novikova.market.core.services.OrderService;
 
 @RestController
 @RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
+@Tag(name = "Заказы", description = "Методы работы с заказами")
 public class OrderController {
     private final OrderService orderService;
 
+    @Operation(
+            summary = "Запрос на создание нового заказа",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "201"
+                    )
+            }
+    )
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createOrder(@RequestHeader String username) {
--- core-service/src/main/java/ru/novikova/market/core/controllers/ProductController.java
+++ core-service/src/main/java/ru/novikova/market/core/controllers/ProductController.java
@@ -1,10 +1,16 @@
 package ru.novikova.market.core.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 import ru.novikova.market.api.dtos.ProductDto;
+import ru.novikova.market.api.exceptions.AppError;
 import ru.novikova.market.api.exceptions.ResourceNotFoundException;
 import ru.novikova.market.core.converters.ProductConverter;
 import ru.novikova.market.core.entities.Product;
@@ -17,10 +23,20 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@Tag(name = "Продукты", description = "Методы работы с продуктами")
 public class ProductController {
     private final ProductService productService;
     private final ProductConverter productConverter;
 
+    @Operation(
+            summary = "Запрос на получение отфильтрованного списка продуктов",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = ProductDto.class))
+                    )
+            }
+    )
     @GetMapping
     public Page<ProductDto> findProducts(
             @RequestParam(required = false, name = "min_price") Integer minPrice,
@@ -35,6 +51,19 @@ public class ProductController {
         return productService.findAll(spec, page - 1).map(productConverter::entityToDto);
     }
 
+    @Operation(
+            summary = "Запрос на получение продукта по id",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = ProductDto.class))
+                    ),
+                    @ApiResponse(
+                            description = "Продукт не найден", responseCode = "404",
+                            content = @Content(schema = @Schema(implementation = AppError.class))
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public ProductDto findProductById(@PathVariable Long id) {
         Product product = productService.findById(id)
@@ -42,6 +71,15 @@ public class ProductController {
         return productConverter.entityToDto(product);
     }
 
+    @Operation(
+            summary = "Запрос на удаление продукта",
+            responses = {
+                    @ApiResponse(
+                            description = "Продукт успешно удален", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = ProductDto.class))
+                    )
+            }
+    )
     @DeleteMapping("/{id}")
     public void deleteProductById(@PathVariable Long id) {
         productService.deleteById(id);
